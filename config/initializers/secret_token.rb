3f7eeef991f4ce0d9a6331947ac265f87a2d883e
Nick Stewart <nicholas4@gmail.com>
2013-02-26 18:06:05 -0800

    rails new run

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aprender::Application.config.secret_token = 'b8ba887070ef6dc15aeb72e0e97e201ebeb404ca14b5c9f946f0332315b5f7bcbf66113b5590deea115207e2b07a8db0d27eb25d575366340b7f17e8de93913e'
