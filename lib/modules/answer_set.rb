09457ab879fabc6b8d40c4fcc750851fb4562b29
Nick Stewart <nicholas4@gmail.com>
2013-04-25 16:58:15 -0600

    added answer_set record_answer

--- lib/modules/answer_set.rb
+++ lib/modules/answer_set.rb
@@ -5,4 +5,12 @@ class AnswerSet
     @answers = _answers.map {|answer_text| Answer.new(answer_text)}
   end
 
+  # def answers_matched
+
+  # end
+
+  def record_answer(answer_text)
+    answers.each {|answer| answer.try_to_match(answer_text) }
+  end
+
 end
--- spec/lib/modules/answer_set_spec.rb
+++ spec/lib/modules/answer_set_spec.rb
@@ -1,9 +1,17 @@
 require 'spec_helper'
 
 describe AnswerSet do
+  let(:_answers) { ["Green", "Blue", "Red"] }
+  let(:answer_set) { described_class.new(_answers) }
+
   context "#initialize" do
-    let(:_answers) { ["Green", "Blue", "Red"] }
-    let(:answer_set) { described_class.new(_answers) }
     specify { answer_set.answers.size.should eq 3 }
+  end
+
+  context "#record_answer" do
+    before { answer_set.record_answer("Blue") }
+    specify { answer_set.answers[0].successfully_matched.should be_false }
+    specify { answer_set.answers[1].successfully_matched.should be_true }
+    specify { answer_set.answers[2].successfully_matched.should be_false  }
   end
 end
