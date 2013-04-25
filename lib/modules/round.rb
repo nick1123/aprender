82af12530e1763e1e6a5268b013182dce8f5eade
Nick Stewart <nicholas4@gmail.com>
2013-04-25 17:31:45 -0600

    round specs

--- lib/modules/round.rb
+++ lib/modules/round.rb
@@ -1,24 +1,19 @@
 class Round
-  attr_reader :answers_added, :completed, :correct_answer, :possible_answers, :question
+  attr_reader :correct_answer, :possible_answers, :question
 
-  def initialize(_question, _correct_answer, _wrong_answers)
-    @answers_added    = 0
-    @completed        = false
-    @correct_answer   = _correct_answer
-    @possible_answers = (_wrong_answers << _correct_answer).shuffle.map {|answer| Answer.new(answer)}
+  def initialize(_question, _possible_answers, _correct_answer)
+    @correct_answer   = ::Answer.new(_correct_answer)
+    @possible_answers = ::AnswerSet.new(_possible_answers)
     @question         = _question
   end
 
-  def add_answer(answer)
-    @answers_added += 1
-    @completed = true if answer == correct_answer
+  def completed?
+    correct_answer.successfully_matched
+  end
 
-    # Record this answer
-    possible_answers.each do |possible_answer|
-      if possible_answer.text == answer
-        possible_answer.previously_selected = true
-      end
-    end
+  def record_answer(answer_text)
+    correct_answer.try_to_match(answer_text)
+    possible_answers.record_answer(answer_text)
   end
 
 end
---
+++ spec/lib/modules/round_spec.rb
@@ -0,0 +1,68 @@
+require 'spec_helper'
+
+describe Round do
+  let(:question) { "Capitol of Utah" }
+  let(:possible_answers) { ["Salt Lake City", "Salem", "Sparta"] }
+  let(:correct_answer) { "Salt Lake City" }
+
+  subject { described_class.new(question, possible_answers, correct_answer) }
+
+  context "#initialize" do
+    specify { subject.question.should eq question }
+  end
+
+  context "#completed?" do
+    context "false" do
+      context "0 answered" do
+        specify { subject.completed?.should be_false }
+      end
+
+      context "1 answered" do
+        before { subject.record_answer("Salem") }
+        specify { subject.completed?.should be_false }
+      end
+
+      context "2 answered" do
+        before do
+          subject.record_answer("Salem")
+          subject.record_answer("Sparta")
+        end
+        specify { subject.completed?.should be_false }
+      end
+    end
+
+    context "false" do
+      context "1 answered" do
+        before { subject.record_answer("Salt Lake City") }
+        specify { subject.completed?.should be_true }
+      end
+
+      context "2 answered" do
+        before do
+          subject.record_answer("Salem")
+          subject.record_answer("Salt Lake City")
+        end
+        specify { subject.completed?.should be_true }
+      end
+
+      context "3 answered" do
+        before do
+          subject.record_answer("Sparta")
+          subject.record_answer("Salem")
+          subject.record_answer("Salt Lake City")
+        end
+        specify { subject.completed?.should be_true }
+      end
+    end
+
+    # before { subject.record_answer("Salem") }
+    # specify { subject.completed?.should be_false }
+  end
+
+
+  context "#record_answer" do
+    before { subject.record_answer("Salem") }
+    specify { subject.completed?.should be_false }
+  end
+
+end
