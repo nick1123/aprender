94c59bc423250eab01de322aa63b066de73e0579
Nick Stewart <nicholas4@gmail.com>
2013-09-28 17:16:08 +0000

    Refactor Answer model

--- lib/modules/answer.rb
+++ lib/modules/answer.rb
@@ -1,13 +1,11 @@
 class Answer
-  attr_accessor :previously_selected
   attr_reader :text
 
   def initialize(_text)
     @text = _text
-    @previously_selected = false
   end
 
   def to_s
-    "Answer: #{text}  previously_selected: #{previously_selected}"
+    return text
   end
 end
